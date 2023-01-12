36f0e0dca98fcd29fcf0e650fb7b63feb8d240cc
youfu hu <568558927@qq.com>
2023-01-12 20:13:22 +0800

    add sleep.c

---
+++ user/sleep.c
@@ -0,0 +1,13 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+void
+main(int argc, char *argv[])
+{
+    if(argc == 1)
+        fprintf(2, "sleep: no argument...\n");
+
+    sleep(atoi(argv[1]));
+
+    exit(0);
+}
